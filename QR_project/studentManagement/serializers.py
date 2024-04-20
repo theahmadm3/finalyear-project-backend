4861f2327e6e5eb84f456a8bc295a5e92616ef1f
unknown <abdullahishuaibumaje.com>
2024-04-20 19:52:46 +0100

    adjusted the serializers again

--- QR_project/studentManagement/serializers.py
+++ QR_project/studentManagement/serializers.py
@@ -1,37 +1,29 @@
-from authentication.models  import CustomUser
-from . models import StudentCourses
+from rest_framework import serializers
 from authentication.models import CustomUser
 from courseManagement.models import Course
-
-from rest_framework import serializers
+from .models import StudentCourses
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        mode=CustomUser
+        model = CustomUser
         fields = ['email']
 
 class CourseSerializer(serializers.ModelSerializer):
-    lecturer=CustomUserSerializer(read_only=True)
+    lecturer = CustomUserSerializer(read_only=True)
+
     class Meta:
-        model=Course
-        fields='__all__'
-
-
+        model = Course
+        fields = '__all__'
 
 class StudentUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['student_id', 'email','first_name','last_name','is_student','level','department','country','phone_number']
-        
-        
-        
-        
+        fields = ['student_id', 'email', 'first_name', 'last_name', 'is_student', 'level', 'department', 'country', 'phone_number']
 
 class StudentCoursesSerializer(serializers.ModelSerializer):
-    enrolled = CourseSerializer(many=True,read_only=True)  # Nested serializer for Course
-    student = CustomUserSerializer(read_only=True)  # Nested serializer for CustomUser
+    course_details =CourseSerializer()  # Nested serializer for the Course model
+    #student = CustomUserSerializer()  # Nested serializer for the CustomUser model
 
     class Meta:
         model = StudentCourses
-        fields = '__all__'  # Include all fields in the serialized output
-  # Add any other fields you want to include
+        fields = ['course_details']
