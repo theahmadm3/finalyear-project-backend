9560760afa29e54b935e03841e3688e577797455
unknown <abdullahishuaibumaje.com>
2024-04-20 17:27:05 +0100

    create courses models

--- QR_project/courseManagement/models.py
+++ QR_project/courseManagement/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from authentication.models import CustomUser
+from django.db.models.constraints import CheckConstraint, Q
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    course_code = models.CharField(max_length=50)
+    lecturer = models.ForeignKey(
+        CustomUser,
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+        limit_choices_to=Q(is_student=False)
+    )
