0ae4246ad94ca861e5229b0becda5208e726adb0
unknown <abdullahishuaibumaje.com>
2024-04-20 11:59:13 +0100

    Added student management urls

--- QR_project/QR_project/urls.py
+++ QR_project/QR_project/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
+    path('api/', include('studentManagement.urls')),
     
 ]
