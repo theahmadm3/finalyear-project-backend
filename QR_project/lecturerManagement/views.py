4de64d56829a3c08600504843ffeba2299289e68
unknown <abdullahishuaibumaje.com>
2024-04-20 21:36:58 +0100

    fixed the endpoint for getting lecturer details and courses

--- QR_project/lecturerManagement/views.py
+++ QR_project/lecturerManagement/views.py
@@ -23,7 +23,7 @@ class GetLecturerCourses(APIView):
                 # Pass the queryset instance to the serializer
                 data_serialized = LecturerCoursesSerializer(instance=lecturer_courses, many=True)
                 return Response({'message': "lecturer courses retrieved successfully",
-                                 'enrolled': data_serialized.data,
+                                 'teaching': data_serialized.data,
                                  'success': True})
             else:
                 return Response({'message': 'No courses found for the lecturer',
@@ -35,17 +35,25 @@ class GetLecturerCourses(APIView):
                              'success': False})
 
 
-class GetLecturerDetails(generics.ListAPIView):
+class GetLecturerDetails(generics.RetrieveAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = lecturerUserSerializer
 
-    def get_queryset(self):
-        return CustomUser.objects.get(id=self.request.user.id,is_student=False)
+    def get_object(self):
+        try:
+            # Retrieve the lecturer user if exists
+            return CustomUser.objects.get(id=self.request.user.id, is_student=False)
+        except CustomUser.DoesNotExist:
+            # Return None if the lecturer is not found
+            return None
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset)
-        return Response({"message":"Lecturer data retrieved successfully","user": serializer.data,"success":True}, status=status.HTTP_200_OK)
+    def retrieve(self, request, *args, **kwargs):
+        lecturer = self.get_object()
 
+        if lecturer is not None:
+            serializer = self.get_serializer(lecturer)
+            return Response({"message": "Lecturer data retrieved successfully", "user": serializer.data, "success": True}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Lecturer not found", "success": False}, status=status.HTTP_404_NOT_FOUND)
 # Create your views here.
