e2b941507dbbc855f1300d06802ecb51d1926f38
unknown <abdullahishuaibumaje.com>
2024-04-20 17:34:02 +0100

    create students model

--- QR_project/studentManagement/models.py
+++ QR_project/studentManagement/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from authentication.models import CustomUser
+from courseManagement.models import Course
+from django.db.models.constraints import CheckConstraint, Q
+class StudentCourses(models.Model):
+    enrolled=models.ForeignKey(Course, null=True, blank=True, on_delete=models.CASCADE)
+    student = models.ForeignKey(
+        CustomUser,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        limit_choices_to=Q(is_student=True)
+    )
+
+
 
 # Create your models here.
