b911819d23897eb9dced93545234cc92cdec433e
unknown <abdullahishuaibumaje.com>
2024-04-20 13:08:24 +0100

    Get student details

--- QR_project/authentication/models.py
+++ QR_project/authentication/models.py
@@ -46,7 +46,7 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    student_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    student_id = models.IntegerField(unique=True, null=True, blank=True)
     email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
     username = models.CharField(max_length=150,null=True, blank=True)
     first_name = models.CharField(max_length=30,null=True, blank=True)
@@ -55,7 +55,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_student = models.BooleanField(default=True)  # Field to distinguish student or lecturer
+    is_student = models.BooleanField(default=True)
+    level=models.IntegerField(blank=True, null=True)
+    department=models.CharField(max_length=150,blank=True,null=True)
+    country=models.CharField(max_length=150,blank=True,null=True)
+    phone_number=models.CharField(max_length=11,blank=True,null=True)
 
     
     objects = CustomUserManager()
