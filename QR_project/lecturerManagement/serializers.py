f43ff752896877770ac5e0378106bca5a0a7b968
unknown <abdullahishuaibumaje.com>
2024-04-20 21:24:09 +0100

    Added serializers objects

---
+++ QR_project/lecturerManagement/serializers.py
@@ -0,0 +1,18 @@
+
+from rest_framework import serializers
+from authentication.models import CustomUser
+from courseManagement.models import Course
+
+
+class lecturerUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email','first_name','last_name','department','country','phone_number']
+
+
+class LecturerCoursesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['title','course_code']
+
+    
