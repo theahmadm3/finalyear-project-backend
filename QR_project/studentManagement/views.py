1d73a01b37a55b268efb601947fb92e69ab7077e
unknown <abdullahishuaibumaje.com>
2024-04-20 14:21:00 +0100

    refactored the get student endpoint

--- QR_project/studentManagement/views.py
+++ QR_project/studentManagement/views.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication 
 from authentication.models  import CustomUser
 from .serializers import StudentUserSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 class GetStudentDetails(generics.ListAPIView):
     authentication_classes = [JWTAuthentication]
@@ -11,3 +13,8 @@ class GetStudentDetails(generics.ListAPIView):
 
     def get_queryset(self):
         return CustomUser.objects.filter(id=self.request.user.id)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"message":"Student data retrieved successfully","data": serializer.data,"success":True}, status=status.HTTP_200_OK)
