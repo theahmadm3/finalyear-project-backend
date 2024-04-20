cacdfab5a390e0682110d2fa713d752318895ce8
unknown <abdullahishuaibumaje.com>
2024-04-20 21:08:02 +0100

    renamed a url

--- QR_project/lecturerManagement/urls.py
+++ QR_project/lecturerManagement/urls.py
@@ -5,7 +5,7 @@ from .views import GetLecturerDetails,GetLecturerCourses
 
 urlpatterns = [
     path('get/lecturer',GetLecturerDetails.as_view(),name='lecturer_details'),
-    path('get/student_courses',GetLecturerCourses.as_view(),name='lecturer_courses'),
+    path('get/lecturer_courses',GetLecturerCourses.as_view(),name='lecturer_courses'),
     
 ]
 
