e0e3e362b4673a5f78dc281c10ab5e761b0fd397
unknown <abdullahishuaibumaje.com>
2024-04-28 00:07:17 +0100

    added the create user in the urls

--- QR_project/authentication/urls.py
+++ QR_project/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import StudentTokenObtainPairView,LecturerTokenObtainPairView
+from .views import StudentTokenObtainPairView,LecturerTokenObtainPairView,CreateUser
 
 
 urlpatterns = [
     path('login/student',StudentTokenObtainPairView.as_view(),name='login_stud'),
     path('login/lecturer',LecturerTokenObtainPairView.as_view(),name='login_lec'),  
+    path('create/user',CreateUser.as_view(),name='create_user'),  
 ]
 
