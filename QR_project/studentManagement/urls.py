89e45f3fc2b98eb2acfb66b46e7f2f69c52b0b4c
unknown <abdullahishuaibumaje.com>
2024-04-28 11:28:59 +0100

    Removed the get student url

--- QR_project/studentManagement/urls.py
+++ QR_project/studentManagement/urls.py
@@ -4,7 +4,6 @@ from .views import GetStudentDetails,GetStudentCourses
 
 
 urlpatterns = [
-    path('get/student',GetStudentDetails.as_view(),name='login_stud'),
     path('get/student_courses',GetStudentCourses.as_view(),name='student_courses'),
     
 ]
