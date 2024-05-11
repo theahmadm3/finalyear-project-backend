5b39de88f0092c94ba8698d9900fecfa50e910f2
Majefashek <abdullahishuaibumaje@gmail.com>
2024-05-11 23:18:16 +0100

    added the get lecturer url

--- QR_project/attendance_app/urls.py
+++ QR_project/attendance_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create/lecturer/attendance',CreateLecturerAttendance.as_view(),name='lecturer_attendance'),    
     path('create/student/attendance',CreateStudentAttendance.as_view(),name='student_attendance'),    
     path('get/student/attendance',GetStudentAttendance.as_view(),name='student_attendance'),    
+    path('get/Lecturer/attendance/<int:course_id>',GetStudentAttendance.as_view(),name='student_attendance'),    
 ]
 
